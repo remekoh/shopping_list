3b512782dd8cbf8f83fe4878b694f2cb296bc5af
rem{e}koh <git@remekoh.me>
2015-05-07 19:54:20 -0500

    Added Javascript/jQuery functionality

---
+++ js/list.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	//add items with enter button
+	$('#addarea').keyup(function(event){
+		if(event.keyCode == 13){
+			event.preventDefault();
+			var item = $('#addarea').val();
+		var text = '<li class="items active"><span>'+item+'</span><div class="item-remove"><span><i class="fa fa-trash"></span></div>'+'<div class="item-bought"><span><i class="fa fa-check"></span></div></li>';
+		$('.items-list').append(text);
+		$('#addarea').val('');
+		}
+	});
+
+	//add items by clicking cart icon
+	$('#submit-item').on('click', function(){
+        var item = $('#addarea').val();
+        var text = '<li class="items active"><span>'+item+'</span><div class="item-remove"><span><i class="fa fa-trash"></span></div>'+'<div class="item-bought"><span><i class="fa fa-check"></span></div></li>';
+        $('.items-list').append(text);
+        $('#addarea').val('');
+    });
+
+    //remove items by clicking trash can icon
+    $(document).on('click','.item-remove', function(){
+        $(this).closest('li.items').fadeOut(400);
+    });
+
+    //mark items as bought by clicking checkmark
+    $(document).on('click','.item-bought',function(){
+        $(this).closest('li.items').removeClass('active');
+        $(this).closest('li.items').addClass('bought');
+        
+    });
+
+})
